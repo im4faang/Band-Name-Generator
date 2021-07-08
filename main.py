dd58f519040593a2cfc726e978682e24b56de7ce
im4faang <86999433+im4faang@users.noreply.github.com>
2021-07-08 22:44:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator Program!")
+# Ask user's city name
+city = input("What's the name of the city you grew up in?\n")
+# Ask user's pet's name
+pet = input("What's your pet's name?\n")
+# Combine two strings
+combined = city + " " + pet
+print("Your band name could be " + combined + ".")
